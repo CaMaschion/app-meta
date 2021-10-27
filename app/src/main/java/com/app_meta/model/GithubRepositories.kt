4d9408d491a205aa06292bb8fe2d7ba3170e3f39
camila <camila@camila-Aspire-A315-42G>
2021-10-27 19:58:03 -0300

    add recyclerview and fix retrofit

---
+++ app/src/main/java/com/app_meta/model/GithubRepositories.kt
@@ -0,0 +1,3 @@
+package com.app_meta.model
+
+data class GithubRepositories(val items: List<Item>)
--- app/src/main/java/com/app_meta/model/Item.kt
+++ app/src/main/java/com/app_meta/model/Item.kt
@@ -1,11 +1,9 @@
 package com.app_meta.model
 
-import java.security.acl.Owner
-
 data class Item(
     val name: String,
     val description: String,
     val forks_count: Int,
     val stargazers_count: Int,
-    var owner: Owner
+    var owner: Author
 )
